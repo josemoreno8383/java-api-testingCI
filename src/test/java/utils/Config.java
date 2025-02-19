34e8c3a127b9cc7f65ca123ded3c4ce62845afe8
josemoreno8383 <50952288+josemoreno8383@users.noreply.github.com>
2025-02-19 15:32:28 -0300

    Update Config.java

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -7,6 +7,6 @@ import java.util.Properties;
 public class Config {
 
     public static String getToken(){
-        return "Bearer "+"";
+        return "Bearer "+"91687ff42491fe0be0f63c1bf63e46ae0b211a5899b185b9431a44d5c35dddb4";
     }
 }
