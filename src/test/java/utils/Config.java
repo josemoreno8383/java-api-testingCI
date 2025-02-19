4ba71bb9229416d34930211d37081358fa1dfecb
josemoreno8383 <50952288+josemoreno8383@users.noreply.github.com>
2025-02-19 15:27:08 -0300

    Update Config.java

--- src/test/java/utils/Config.java
+++ src/test/java/utils/Config.java
@@ -6,16 +6,7 @@ import java.util.Properties;
 
 public class Config {
 
-    private static final String CONFIG_PATH="src/test/resources/config.properties";
-
     public static String getToken(){
-        Properties properties = new Properties();
-        try (FileInputStream file = new FileInputStream(CONFIG_PATH)) {
-            properties.load(file);
-            return "Bearer "+properties.getProperty("GOREST_TOKEN");
-        }catch (IOException e){
-            throw new RuntimeException("Error al configurar el token");
-        }
-
+        return "Bearer "+"";
     }
 }
